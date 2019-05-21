3df5840c2cb5bdcc868d15b910040a48f7dbdd74
Sario <cesargonfer@gmail.com>
2019-05-21 15:31:09 +0200

    Add multiprocessing.

--- py3_aggregator.py
+++ py3_aggregator.py
@@ -1,10 +1,41 @@
 import csv
+import multiprocessing
 import os
 import sys
 
 from newspaper import Article
+import requests
+
+
+in_folder = None
+out_folder = None
+
+def process_raw(f):
+    with open(os.path.join(in_folder, f)) as f_in, \
+            open(os.path.join(out_folder, f), 'w') as f_out:
+        reader = csv.reader(f_in, delimiter='\t')
+        writer = csv.writer(f_out, delimiter=',')
+        for row in reader:
+            title = row[1]
+            url = row[2]
+            cat = row[4]
+            id_ = row[5]
+            request = requests.get(url)
+            if request.status_code != 200:
+                print('The url {} does not exist. Skip.'.format(url))
+                continue
+            try:
+                article = Article(url)
+                article.download()
+                article.parse()
+
+                writer.writerow([title, article.text, cat, id_])
+            except Exception as _:
+                print('The url {} cannot be processed. Skip.'.format(url))
+
 
 def main():
+    global in_folder, out_folder
     in_folder = sys.argv[1]
     out_folder = sys.argv[2]
 
@@ -15,26 +46,9 @@ def main():
     out_files = set(os.listdir(out_folder))
 
     raw_files -= out_files
-
-    for f in raw_files:
-        print(f)
-        with open(os.path.join(in_folder, f)) as f_in, \
-                open(os.path.join(out_folder, f), 'w') as f_out:
-            reader = csv.reader(f_in, delimiter='\t')
-            writer = csv.writer(f_out, delimiter=',')
-            for row in reader:
-                title = row[1]
-                url = row[2]
-                cat = row[4]
-                id_ = row[5]
-                try:
-                    article = Article(url)
-                    article.download()
-                    article.parse()
-
-                    writer.writerow([title, article.text, cat, id_])
-                except Exception as _:
-                    print('The url {} cannot be processed. Skip.'.format(url))
+    with multiprocessing.Pool(
+            processes=max(1, multiprocessing.cpu_count() - 2)) as pool:
+        pool.map(process_raw, raw_files)
 
 
 if __name__ == "__main__":
