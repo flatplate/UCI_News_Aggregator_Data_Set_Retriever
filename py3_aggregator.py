7606a06024007a9c5f57ae15f2fa199528bf49e9
Ural Bayhan <uralbayhan0@gmail.com>
2021-07-19 12:57:28 +0200

    Add concurrent requests for each process
    Using concurrent.futures request threads per process can be increased without
    much overhead.
    
    Also increased max process count

--- py3_aggregator.py
+++ py3_aggregator.py
@@ -2,39 +2,65 @@ import csv
 import multiprocessing
 import os
 import sys
+import concurrent.futures
 
 from newspaper import fulltext
 import requests
 
 
-in_folder = None
-out_folder = None
+def get_content(url):
+    try:
+        request = requests.get(url, timeout=30)
+        if request.status_code != 200:
+            print('The url {} does not exist. Skip.'.format(url))
+            return ""
+        return request.text
+    except requests.exceptions.Timeout as _:
+        print('The url {} timeout. Skip.'.format(url))
+    except Exception as e:
+        print('The url {} cannot be processed. Skip.'.format(url), str(e))
+    return ""
 
-def process_raw(f):
-    with open(os.path.join(in_folder, f)) as f_in, \
-            open(os.path.join(out_folder, f), 'w') as f_out:
+def get_fulltext_catch_exceptions(text):
+    try:
+        return fulltext(text)
+    except Exception as e:
+        print('The text cannot be processed. Skip.', str(e))
+        return ""
+
+def process_raw(args):
+    f = args[0]
+    in_folder = args[1]
+    out_folder = args[2]
+    in_file = os.path.join(in_folder, f)
+    out_file = os.path.join(out_folder, f)
+    
+    with open(in_file, "r", encoding="utf-8") as f_in, open(out_file, 'w', encoding="utf-8") as f_out:
         reader = csv.reader(f_in, delimiter='\t')
         writer = csv.writer(f_out, delimiter=',')
-        for row in reader:
-            title = row[1]
-            url = row[2]
-            cat = row[4]
-            id_ = row[5]
-            try:
-                request = requests.get(url, timeout=30)
-                if request.status_code != 200:
-                    print('The url {} does not exist. Skip.'.format(url))
-                    continue
-                text = fulltext(request.text)
-                writer.writerow([title, text, cat, id_])
-            except requests.exceptions.Timeout as _:
-                print('The url {} timeout. Skip.'.format(url))
-            except Exception as _:
-                print('The url {} cannot be processed. Skip.'.format(url))
 
+        articles = [{
+            "title": row[1],
+            "content": "",
+            "cat": row[4],
+            "id_": row[5],
+            "url": row[2]
+        } for row in reader]
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=25) as concurrentPool:
+            contents = [get_fulltext_catch_exceptions(text) for text in concurrentPool.map(get_content, [article["url"] for article in articles])]
+
+        for article, content in zip(articles, contents):
+            article["content"] = content
+
+        writer.writerows([article.values() for article in articles])
 
 def main():
     global in_folder, out_folder
+
+    if len(sys.argv) < 3:
+        print("Usage: python UCI_news_aggregator_DS_retriever.py <input_folder_name> <output_folder_name>")
+
     in_folder = sys.argv[1]
     out_folder = sys.argv[2]
 
@@ -46,7 +72,7 @@ def main():
 
     raw_files -= out_files
     with multiprocessing.Pool(
-            processes=max(1, multiprocessing.cpu_count() - 2)) as pool:
+            processes=max(1, multiprocessing.cpu_count())) as pool:
         pool.map(process_raw, raw_files)
 
 
